144ae60d1cf56fe54648886bd02e1c633b912608
System <none>
2017-07-30 17:14:20 +0000

    Checkpoint

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,34 @@
 var express = require('express');
 var multer  = require('multer');
-// Create a storage object with a given configuration 
+var url = require('url');
+ 
+var settings = {
+    "host": 'mongodb://will:password@ds125623.mlab.com:25623/',
+    "port": 25623,
+    "database": 'fcc-backend'
+};
+ 
+var connectionString = url.format({
+    protocol: 'mongodb',
+    slashes: true,
+    hostname: settings.host,
+    port: settings.port,
+    pathname: settings.database
+});
+ 
+var storage = require('multer-gridfs-storage')({
+    url: connectionString
+});
+var upload = multer({ storage: storage });
+
+
+
+// // Create a storage object with a given configuration 
 // var storage = require('multer-gridfs-storage')({
-//    url: 'mongodb://will:password@ds125623.mlab.com:25623/fcc-backend'
-// });
-// Set multer storage engine to the newly created object 
-var upload = multer({ storage: "/upload" });
+//     url: 'mongodb://will:password@ds125623.mlab.com:25623/fcc-backend'
+//  });
+// // Set multer storage engine to the newly created object 
+// var upload = multer({ storage: "/upload" });
   
 // Upload your files as usual 
 var sUpload = upload.single('file');
